0e906aa8301593e344307e611c209901c2bfad97
250166 <250166@newuu.uz>
2025-10-24 10:18:53 +0500

    asdf

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+//problem 1
+// int powerFunction(int base, int exp) {
+//     if (exp == 0) {return 1;}
+//     else {return base * powerFunction(base, exp - 1);}
+// }
+// int main() {
+//     int power, exp;
+//     cin >> power >> exp;
+//     cout << powerFunction(power, exp) << endl;
+// }
+
+//problem 2
+// int reverse(int number, int reversed = 0) {
+//     if (number == 0) {return reversed;}
+//     else {
+//         int digit = number % 10;
+//         reversed = reversed * 10 + digit;
+//         return reverse(number / 10, reversed);
+//         }
+// }
+// int main() {
+//     int number;
+//     cin >> number;
+//     cout << reverse(number);
+// }
+
+//problem 3
+bool palindromeChecker(int number, int reversed = 0) {
+    static int num = number;
+    if (number < 0) {
+        return false;
+    }
+    else {
+        int digit = number % 10;
+        reversed = reversed * 10 + digit;
+        return palindromeChecker(number % 10, reversed);
+    }
+
+}
+int main() {
+    int number;
+    cin >> number;
+    cout << palindromeChecker(number);
+}
+
+//problem 4
+// int primeChecker(int number) {
+//     if (number % 2 == 0) {
+//         return 0;
+//     }
+//     else {
+//         return primeChecker(number);
+//     }
+// }
+// int main() {
+//     int number;
+//     cin >> number;
+// }
